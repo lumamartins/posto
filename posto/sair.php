25cb0e8d42240bb968eb647dc493d2a0500792ba
lumamartins <133801111+lumamartins@users.noreply.github.com>
2023-09-12 12:24:17 -0300

    Delete posto/sair.php

--- posto/sair.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// pagina para sair do sistema onde estao salvos os dados do banco de dados dos pacientes
-    session_start();
-    unset($_SESSION['usuario']);
-    unset($_SESSION['senha']);
-    header("Location: login.php");
-?>
